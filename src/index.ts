a7e12136ff0e4d4f9477d0a4a229ea2662afed2b
AliHassanFidati <ali.hassan@gx2.com.br>
2024-04-28 12:46:47 -0400

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * from './is-not-empty-validator.decorator';
 export * from './length-range-validator.decorator';
 export * from './max-length-validator.decorator';
+export * from './max-validator.decorator';
 export * from './min-length-validator.decorator';
+export * from './min-validator.decorator';
+export * from './range-validator.decorator';
+
